3f006dcbe0e9caf3aef015f7e62ba3a7f260b3fb
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-14 23:20:53 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,7 +13,6 @@ def input_to_index(user_input)
 end
 
 def move(board, index, character = "X")
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   board[index] = character
   return board
 end
